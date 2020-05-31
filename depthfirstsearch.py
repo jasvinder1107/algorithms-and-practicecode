1ddad3ac7897872fb53d86b21eff91655fb24e7d
jasvinder singh kwatra <jasvinder.kwatra@gmail.com>
2020-05-31 18:45:55 -0500

    depth first search

---
+++ depthfirstsearch.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+
+def depthfirstsearch(g,firstelement):
+    """
+     This function implements depth first search given a graph g and source element as firstelement.
+
+    """
+    visited=[]
+    stack=[firstelement]
+    while stack:
+        node=stack[-1]
+        if node not in visited:
+            visited.append(node)
+        delete_from_stack=True
+        neighbours=g[node]
+        for n in neighbours:
+            if n not in visited:
+                stack.extend(n)
+                delete_from_stack=False
+                break
+        if delete_from_stack:
+            stack.pop()
+    return visited
+
+
+if __name__ == "__main__":
+
+ graph2= {'A': ['B','C','E'],
+          'B': [ 'D','F'],
+          'C': ['G'],
+          'D': ['B'],
+          'E': ['F'],
+          'F': ['E'],
+          'G': ['C']}
+
+ print("DFS sequence %s" % depthfirstsearch(graph2,'A'))
